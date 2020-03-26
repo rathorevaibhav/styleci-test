8c3161cc39c03547981d14e5265a214f76f40e34
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-03-26 16:33:32 +0530

    more fixes

---
+++ styleci.php
@@ -0,0 +1,14 @@
+<?php
+
+class StyleCI {
+    public function main() {
+        if (true)
+        {
+        	// this is fine
+        }
+        else
+        {
+        	// this is not fine
+        }
+    }
+}
