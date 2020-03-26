1756e430f24a4237147ce933e6b52211c10e2187
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-03-26 17:16:27 +0530

    testing

--- styleci.php
+++ styleci.php
@@ -1,12 +1,12 @@
 <?php
 
-class styleci
-{
-    public function main()
-    {
-        if (true) {
+class StyleCI {
+    public function main() {
+        if (true)
+        {
             // this is fine
-        } else {
+        } else
+        {
             // this is not fine
         }
     }
